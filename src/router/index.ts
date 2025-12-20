979da83e893e13455f3c486eda5421a32fafb225
Florian Schmid <melonencool@gmail.com>
2025-12-20 17:52:18 +0100

    add 404 page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomeView, },
     { path: '/project/floslabs', name: 'floslabs', component: () => import('../views/projects/floslabs.vue'), },
+
+    { path: '/:pathMatch(.*)*', component: NotFoundView },
   ],
 })
 
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="w-full h-full flex flex-col justify-center items-center space-y-2">
+  <div class="font-[Murecho] text-2xl text-white space-x-5 flex flex-row">
+    <h1>This page does not exist...</h1>
+    <h1>yet</h1>
+  </div>
+  <RouterLink to="/" class="text-blue-400 hover:cursor-pointer hover:underline">
+      Back to the projects
+  </RouterLink>
+</div>
+</template>
